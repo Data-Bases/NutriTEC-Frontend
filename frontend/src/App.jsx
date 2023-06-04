8002c02a22dcbf484ca81b66074e72f2d8aebbc7
Andrés Martínez Vargas <martinezandres@estudiantec.cr>
2023-06-03 20:43:25 -0600

    Creación de componentes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,12 @@ import SignUp from "./components/SingUp";
 import Welcome from "./components/welcome";
 import SignUpChoose from "./components/SingUpChoose";
 
+import ProductList from './components/ProductList';
+
+import ProductManagement from './components/ProductManagement';
+
+import TabBar from './components/NavigationBar';
+
 import { pathToRegexp } from "path-to-regexp";
 import { Router, Switch, Route, Link, useRoute } from "wouter";
 import makeCachedMatcher from "wouter/matcher";
@@ -21,34 +27,186 @@ import makeCachedMatcher from "wouter/matcher";
  * @return {{ keys: [], regexp: RegExp }}
  */
 const convertPathToRegexp = (path) => {
-  let keys = [];
+    let keys = [];
 
-  // we use original pathToRegexp package here with keys
-  const regexp = pathToRegexp(path, keys);
-  return { keys, regexp };
+    // we use original pathToRegexp package here with keys
+    const regexp = pathToRegexp(path, keys);
+    return { keys, regexp };
 };
 
 // signature of the matcher fn: (pattern, path) => [success, params]
 const customMatcher = makeCachedMatcher(convertPathToRegexp);
 
+const productos = [
+    {
+        nombre: 'producto 1',
+        identificador: '1001',
+        energia: 'XXXXXXXXXX',
+        grasa: 'XXXXXXXXXX',
+        sodio: 'XXXXXXXXXX',
+        carbohidratos: 'XXXXXXXXXX',
+        proteina: 'XXXXXXXXXX',
+        vitaminas: 'XXXXXXXXXXX',
+        calcio: 'XXXXXXXXXXX',
+        hierro: 'XXXXXXXXXXX'
+    },
+    {
+        nombre: 'producto 2',
+        identificador: '1002',
+        energia: 'XXXXXXXXXX',
+        grasa: 'XXXXXXXXXX',
+        sodio: 'XXXXXXXXXX',
+        carbohidratos: 'XXXXXXXXXX',
+        proteina: 'XXXXXXXXXX',
+        vitaminas: 'XXXXXXXXXXX',
+        calcio: 'XXXXXXXXXXX',
+        hierro: 'XXXXXXXXXXX'
+    },
+    {
+        nombre: 'producto 3',
+        identificador: '1003',
+        energia: 'XXXXXXXXXX',
+        grasa: 'XXXXXXXXXX',
+        sodio: 'XXXXXXXXXX',
+        carbohidratos: 'XXXXXXXXXX',
+        proteina: 'XXXXXXXXXX',
+        vitaminas: 'XXXXXXXXXXX',
+        calcio: 'XXXXXXXXXXX',
+        hierro: 'XXXXXXXXXXX'
+    },
+    {
+        nombre: 'producto 4',
+        identificador: '1004',
+        energia: 'XXXXXXXXXX',
+        grasa: 'XXXXXXXXXX',
+        sodio: 'XXXXXXXXXX',
+        carbohidratos: 'XXXXXXXXXX',
+        proteina: 'XXXXXXXXXX',
+        vitaminas: 'XXXXXXXXXXX',
+        calcio: 'XXXXXXXXXXX',
+        hierro: 'XXXXXXXXXXX'
+    },
+    {
+        nombre: 'producto 5',
+        identificador: '1005',
+        energia: 'XXXXXXXXXX',
+        grasa: 'XXXXXXXXXX',
+        sodio: 'XXXXXXXXXX',
+        carbohidratos: 'XXXXXXXXXX',
+        proteina: 'XXXXXXXXXX',
+        vitaminas: 'XXXXXXXXXXX',
+        calcio: 'XXXXXXXXXXX',
+        hierro: 'XXXXXXXXXXX'
+    },
+    {
+        nombre: 'producto 6',
+        identificador: '1006',
+        energia: 'XXXXXXXXXX',
+        grasa: 'XXXXXXXXXX',
+        sodio: 'XXXXXXXXXX',
+        carbohidratos: 'XXXXXXXXXX',
+        proteina: 'XXXXXXXXXX',
+        vitaminas: 'XXXXXXXXXXX',
+        calcio: 'XXXXXXXXXXX',
+        hierro: 'XXXXXXXXXXX'
+    },
+    {
+        nombre: 'producto 7',
+        identificador: '1007',
+        energia: 'XXXXXXXXXX',
+        grasa: 'XXXXXXXXXX',
+        sodio: 'XXXXXXXXXX',
+        carbohidratos: 'XXXXXXXXXX',
+        proteina: 'XXXXXXXXXX',
+        vitaminas: 'XXXXXXXXXXX',
+        calcio: 'XXXXXXXXXXX',
+        hierro: 'XXXXXXXXXXX'
+    },
+    {
+        nombre: 'producto 8',
+        identificador: '1008',
+        energia: 'XXXXXXXXXX',
+        grasa: 'XXXXXXXXXX',
+        sodio: 'XXXXXXXXXX',
+        carbohidratos: 'XXXXXXXXXX',
+        proteina: 'XXXXXXXXXX',
+        vitaminas: 'XXXXXXXXXXX',
+        calcio: 'XXXXXXXXXXX',
+        hierro: 'XXXXXXXXXXX'
+    },
+    {
+        nombre: 'producto 9',
+        identificador: '1009',
+        energia: 'XXXXXXXXXX',
+        grasa: 'XXXXXXXXXX',
+        sodio: 'XXXXXXXXXX',
+        carbohidratos: 'XXXXXXXXXX',
+        proteina: 'XXXXXXXXXX',
+        vitaminas: 'XXXXXXXXXXX',
+        calcio: 'XXXXXXXXXXX',
+        hierro: 'XXXXXXXXXXX'
+    },
+    {
+        nombre: 'producto 10',
+        identificador: '10010',
+        energia: 'XXXXXXXXXX',
+        grasa: 'XXXXXXXXXX',
+        sodio: 'XXXXXXXXXX',
+        carbohidratos: 'XXXXXXXXXX',
+        proteina: 'XXXXXXXXXX',
+        vitaminas: 'XXXXXXXXXXX',
+        calcio: 'XXXXXXXXXXX',
+        hierro: 'XXXXXXXXXXX'
+    },
+    {
+        nombre: 'producto 11',
+        identificador: '10011',
+        energia: 'XXXXXXXXXX',
+        grasa: 'XXXXXXXXXX',
+        sodio: 'XXXXXXXXXX',
+        carbohidratos: 'XXXXXXXXXX',
+        proteina: 'XXXXXXXXXX',
+        vitaminas: 'XXXXXXXXXXX',
+        calcio: 'XXXXXXXXXXX',
+        hierro: 'XXXXXXXXXXX'
+    },
+    {
+        nombre: 'producto 12',
+        identificador: '10012',
+        energia: 'XXXXXXXXXX',
+        grasa: 'XXXXXXXXXX',
+        sodio: 'XXXXXXXXXX',
+        carbohidratos: 'XXXXXXXXXX',
+        proteina: 'XXXXXXXXXX',
+        vitaminas: 'XXXXXXXXXXX',
+        calcio: 'XXXXXXXXXXX',
+        hierro: 'XXXXXXXXXXX'
+    }
+];
+
+
 function App() {
     const [count, setCount] = useState(0);
 
     return (
-        <Router matcher={customMatcher}>
-            <div>
-                <Switch>
-                    <Route path="/" component={Welcome} />
-                    <Route path="/login"><Login/></Route>
-                    <Route path="/signup"><SignUpChoose/></Route>
-                    <Route path="/signup/nutricionista-1"><SignUp pantalla="N1"/></Route>
-                    <Route path="/signup/nutricionista-2"><SignUp pantalla="N2"/></Route>
-                    <Route path="/signup/cliente-1"><SignUp pantalla="C1"/></Route>
-                    <Route path="/signup/cliente-2"><SignUp pantalla="C2"/></Route>
-                    <Route path="/:anything">404 Página no encontrada</Route>
-                </Switch>
-            </div>
-        </Router>
+        <>
+            <ProductManagement objetosBD={productos}></ProductManagement>
+            <TabBar></TabBar>
+        </>
+        // <Router matcher={customMatcher}>
+        //     <div>
+        //         <Switch>
+        //             <Route path="/" component={Welcome} />
+        //             <Route path="/login"><Login/></Route>
+        //             <Route path="/signup"><SignUpChoose/></Route>
+        //             <Route path="/signup/nutricionista-1"><SignUp pantalla="N1"/></Route>
+        //             <Route path="/signup/nutricionista-2"><SignUp pantalla="N2"/></Route>
+        //             <Route path="/signup/cliente-1"><SignUp pantalla="C1"/></Route>
+        //             <Route path="/signup/cliente-2"><SignUp pantalla="C2"/></Route>
+        //             <Route path="/:anything">404 Página no encontrada</Route>
+        //         </Switch>
+        //     </div>
+        // </Router>
     );
 }
 
---
+++ frontend/src/components/RecipeCreator.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import { Form, Button, Dropdown } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function RecipeCreator({ producto }) {
+    const [porcion, setPorcion] = useState('');
+    const [gramos, setGramos] = useState('');
+    const [recipeProducts, setRecipeProducts] = useState([]);
+
+    const handleAddProductClick = () => {
+        const newProduct = {
+            product: producto,
+            porcion: porcion,
+            gramos: gramos
+        };
+
+        setRecipeProducts([...recipeProducts, newProduct]);
+        setPorcion('');
+        setGramos('');
+
+        console.log(recipeProducts)
+    };
+
+    return (
+        <>
+            <hr />
+
+            <div className="d-flex" style={{ justifyContent: 'start', alignItems: 'center' }}>
+                <div className="d-flex" style={{ marginRight: '10px', justifyContent: 'center', width: '500px', padding: '10px', borderRadius: '5px', boxShadow: '5px 5px 10px' }}>
+                    {producto ? <div>{producto.nombre}</div> : <div>Producto</div>}
+                </div>
+                <Form.Control
+                    style={{ marginRight: '10px' }}
+                    placeholder="Porción"
+                    value={porcion}
+                    onChange={(e) => setPorcion(e.target.value)}
+                />
+                <Form.Control
+                    style={{ marginRight: '10px' }}
+                    placeholder="g"
+                    value={gramos}
+                    onChange={(e) => setGramos(e.target.value)}
+                />
+                <Button onClick={handleAddProductClick} style={{ background: '#1382C9' }}> + </Button>
+
+                <Dropdown style={{marginLeft:'10px'}}>
+                    <Dropdown.Toggle variant="primary" id="dropdown-basic"> Receta </Dropdown.Toggle>
+                    <Dropdown.Menu >
+                        {recipeProducts.map((recipeProduct, index) => (
+                            <Dropdown.Item key={index}>{recipeProduct.product.nombre + ", porciones: " + recipeProduct.porcion + ", gramos: " + recipeProduct.gramos}</Dropdown.Item>
+                        ))}
+                    </Dropdown.Menu>
+                </Dropdown>
+            </div>
+
+
+        </>
+    );
+}
+
+export default RecipeCreator;
